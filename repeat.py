8eda33164870b9e1e31887163516f7aba06d50f5
wayne <278972894@qq.com>
2016-05-10 17:47:50 +0800

    change

--- repeat.py
+++ repeat.py
@@ -3,11 +3,23 @@
 # File Name: repeat.py
 #########################################################################
 #!/bin/python
-def greater():
+def greeter():
     print "hello"
 
 def repeat(f, times):
     for i in range(times):
         f()
 
-repeat(greater, 4)
+repeat(greeter, 4)
+
+print "-"*30
+
+def print_call(fn):
+    def fn_wrap(*args, **kwargs): #take any arguments
+        print "Calling %s" % (fn.func_name)
+        return fn(*args, **kwargs) #pass any arguments to fn()
+    return fn_wrap
+
+
+greeter = print_call(greeter) #wrap greeter
+repeat(greeter, 3)
